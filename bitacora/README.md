854e4f7298ac6bd959f67b893c10e95eec3d40a2
johnnycubides <jgcubidesc@unal.edu.co>
2018-06-21 13:11:32 -0500

    bitacora to document commands used in a folder

---
+++ bitacora/README.md
@@ -0,0 +1,25 @@
+# Bitacora.sh #
+
+El objetivo de éste script es permitir guardar una secuencia de comandos que
+sean usados en un directorio en la creación de un proyecto. En general cuando
+se abandona un proyecto por bastante tiempo, se hace un poco complicado recordar
+los comandos a usar para una tarea específica.
+
+Es una forma de hacer documentación; pero es complementaria y debe usarse por ejemplo
+para simplificar las cosas markdown, o restructuredtext entre otros.
+
+## Instalación ##
+
+### Dar permisos de ejecución al programa ###
+
+```bash
+chmod +x bitacora.sh
+```
+
+### Instalar para usar en cualquier lugar de desde una terminal ####
+
+```bash
+sudo cp bitacora.sh /usr/local/bin/
+```
+
+Regards Johnny
---
+++ bitacora/bitacora.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Instructions
+# chmod +x bitacora.sh
+# sudo cp bitacora.sh /usr/local/bin/
+# bitacora.sh "command" "observation"
+#Author: Johnny Cubides
+#e-mail: jgcubidesc@gmail.com
+#date: Thu Jun 21 11:39:03 -05 2018
+
+
+# Permite guardar información acerca de comandos usados
+if [ "$1" = "-h" ] || [ "$1" = "" ] || [ "$1" = "--help" ];
+then
+    printf "Help for this command bitacora.sh\n"
+    printf "\tbitacora.sh \"command\" \"observation\"\n"
+    printf "\tbitacora.sh [Options] \"command\" \"observation\"\n"
+    printf "\t[Options]\n"
+    printf "\t\t-t\tthis separator\\t\n\t\t-,\tthis separator , (this default)\n"
+    printf "\tbitacora.sh [Parameters]\n"
+    printf "\t[Parameters]\n"
+    printf "\t\t-i\tstart bitacora.csv\n"
+    printf "\t\t-v\tview content\n"
+    printf "\t\t-h,--help\tHelp\n"
+    printf "\nRegards Johnny.\n"
+elif [ "$1" = "-v" ];
+then
+    less bitacora.csv
+elif [ "$1" = "-i" ];
+then
+    touch bitacora.csv && printf "`pwd`\ndate,command,observations\n" >> bitacora.csv
+elif [ "$1" = "-t" ];
+then
+    #separator (\t)
+    printf "`date`\t$2\t$3\n" >> bitacora.csv
+    printf "Task successful `whoami`\n"
+elif [ "$1" = "-," ];
+then
+    #separator (,)
+    printf "`date`,$2,$3\n" >> bitacora.csv
+    printf "Task successful `whoami`\n"
+else
+    printf "`date`,$1,$2\n" >> bitacora.csv
+    printf "Task successful `whoami`\n"
+fi
