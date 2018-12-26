75a58c199194a9b173909399b6ea9cfa038e7a57
johnnycubides <jgcubidesc@gmail.com>
2018-12-26 15:22:49 -0500

    new dir and installator for bitacora.sh

---
+++ bash/bitacora/install_bitacora_sh.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+#
+# Brief:	Instalador de script bitacora.sh
+# Author: Johnny Cubides
+# e-mail: johnnycubides@catalejoplus.com
+# date: Wednesday 26 December 2018
+
+if [ ! -L /usr/local/bin/bitacora.sh ]; then
+  sudo ln -s bitacora.sh /usr/local/bin/bitacora.sh
+else
+  echo `ls -l /usr/local/bin/bitacora.sh`" simbolic link exist"
+  read -p "¿Desea sobreescribir el enlace a bitacora.sh? y/n: " -r temp
+  if [ "$temp" = "y" ];
+  then
+    sudo rm -v /usr/local/bin/bitacora.sh
+    sudo ln -s bitacora.sh /usr/local/bin/bitacora.sh
+    echo `ls -l /usr/local/bin/bitacora.sh`" simbolic link created"
+  fi
+fi
