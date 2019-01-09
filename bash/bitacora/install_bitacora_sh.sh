c5cead4538fc9afd2a08b2fa3584177fa42af4d6
johnnycubides <jgcubidesc@gmail.com>
2019-01-09 12:57:40 -0500

    symbolic link relative

--- bash/bitacora/install_bitacora_sh.sh
+++ bash/bitacora/install_bitacora_sh.sh
@@ -6,14 +6,14 @@
 # date: Wednesday 26 December 2018
 
 if [ ! -L /usr/local/bin/bitacora.sh ]; then
-  sudo ln -s bitacora.sh /usr/local/bin/bitacora.sh
+  sudo ln -sr bitacora.sh /usr/local/bin/bitacora.sh
 else
   echo `ls -l /usr/local/bin/bitacora.sh`" simbolic link exist"
   read -p "¿Desea sobreescribir el enlace a bitacora.sh? y/n: " -r temp
   if [ "$temp" = "y" ];
   then
     sudo rm -v /usr/local/bin/bitacora.sh
-    sudo ln -s bitacora.sh /usr/local/bin/bitacora.sh
+    sudo ln -sr bitacora.sh /usr/local/bin/bitacora.sh
     echo `ls -l /usr/local/bin/bitacora.sh`" simbolic link created"
   fi
 fi
