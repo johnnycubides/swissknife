e32fe7c544259e25a86afaa903481ddc53cc7150
Johnny Cubides <jgcubidesc@gmail.com>
2022-09-10 00:07:26 -0500

    fix: update docs

--- vim/nvimrc-acer/local_bundles.vim
+++ vim/nvimrc-acer/local_bundles.vim
@@ -2,3 +2,4 @@ Plug 'ajh17/vimcompletesme'
 Plug 'jszakmeister/markdown2ctags'
 Plug 'iamcco/markdown-preview.nvim', { 'do': { -> mkdp#util#install() } }
 Plug 'mzlogin/vim-markdown-toc'
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
--- vim/nvimrc-acer/sh-config-nvimrc.sh
+++ vim/nvimrc-acer/sh-config-nvimrc.sh
@@ -3,6 +3,8 @@
 rm -f ~/.config/nvim/init.vim
 rm -f ~/.config/nvim/local_init.vim
 rm -f ~/.config/nvim/local_bundles.vim
+rm -f ~/config/nvim/coc-settings.json
 ln -s `pwd`/generate.vim ~/.config/nvim/init.vim
 ln -s `pwd`/local_init.vim ~/.config/nvim/local_init.vim
 ln -s `pwd`/local_bundles.vim ~/.config/nvim/local_bundles.vim
+ln -s `pwd`/coc-settings.json ~/.config/nvim/coc-settings.json
