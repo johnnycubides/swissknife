637cbcdc59d01ebe39877329a9bb59f86912fba4
Johnny Germán Cubides Castro <jgcubidesc@gmail.com>
2024-03-14 12:25:12 -0500

    Update install-all.bash

--- bash/installs/install-all.bash
+++ bash/installs/install-all.bash
@@ -1,9 +1,11 @@
-GITUI_VERSION=0.25.1
+DP=gitPackges
+GITUI_VERSION=v0.25.1
+
 
 sudo apt update
 sudo apt install git -y
 
-mkdir -p ~/gitPackges/ && cd ~/gitPackages
+mkdir -p ~/$DP/ && cd ~/$DP
 wget -O "gitui.tar.gz" "https://github.com/extrawurst/gitui/releases/download/$GITUI_VERSION/gitui-linux-musl.tar.gz"
 tar xvf gitui.tar.gz
 chmod +x gitui
