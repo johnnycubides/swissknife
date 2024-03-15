2719da00789f24cf4d204cdb650cb59a63ea1644
Johnny Cubides <jgcubidesc@gmail.com>
2024-03-14 22:19:08 -0500

    update documentation

---
+++ bash/installs/tmux/tmux-install.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DP=~/gitPackages
+APP=$DP/tmux
+
+install() {
+	sudo apt update
+	sudo apt install tmux -y
+}
+
+config() {
+	rm -rf ~/.config/tmux
+	rm -rf $APP
+	mkdir -p $DP
+	cd $DP
+	git clone https://github.com/gpakosz/.tmux.git tmux
+	cd $APP
+	mkdir -p ~/.config/tmux
+	ln -s $APP/.tmux.conf ~/.config/tmux/tmux.conf
+	cp .tmux.conf.local ~/.config/tmux/tmux.conf.local
+}
+
+# install
+config
