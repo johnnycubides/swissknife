3e55060eb6d0ed2e28551bd331b2ed2758fd0902
Johnny Cubides <jgcubidesc@gmail.com>
2024-03-14 21:26:15 -0500

    update docs

---
+++ bash/installs/latex/latex-install.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+install() {
+	sudo apt update
+	sudo apt install \
+		texlive-latex-recommended \
+		texlive-latex-extra \
+		texlive-lang-spanish \
+		texlive-fonts-extra \
+		texlive-bibtex-extra \
+		inkscape \
+		-y
+}
+
+install
