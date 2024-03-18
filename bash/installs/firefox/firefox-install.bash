0a86d07ef2e214970e49ab16a5479d157fd5196a
Johnny Cubides <jgcubidesc@gmail.com>
2024-03-18 18:06:35 -0500

    update firefox script install

---
+++ bash/installs/firefox/firefox-install.bash
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(pwd)
+
+remove-firefox-esr() {
+	sudo apt remove firefox-esr -y
+}
+
+DOWNLOAD=https://download.mozilla.org/?product=firefox-latest-ssl &
+os=linux64 &
+lang=en-US &
+_gl=1*oj7wrq*_ga*MTMwNTcxODcwNC4xNzEwNzk4NDc5*_ga_MQ7767QQQW*MTcxMDc5ODQ3OC4xLjEuMTcxMDc5ODQ4My4wLjAuMA..
+
+DP=~/gitPackages
+DAPP=$DP/firefox
+DOWNLOAD="https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US"
+
+install() {
+	rm -rf $DAPP
+	sudo rm /usr/local/bin/firefox
+	mkdir -p $DAPP
+	cd $DAPP
+	wget -O firefox.tar.bz2 $DOWNLOAD
+	tar xvjf firefox.tar.bz2
+	rm -f firefox.tar.bz2
+	cd firefox
+	sudo ln -sr firefox /usr/local/bin/firefox
+}
+
+update-firefox() {
+	cd $DAPP
+	wget -O firefox.tar.bz2 $DOWNLOAD
+	tar xvjf firefox.tar.bz2
+	rm -f firefox.tar.bz2
+}
+
+desktop-file() {
+
+	PATH_LOGO=$DAPP/firefox/browser/chrome/icons/default/default128.png
+	RESOURCE_NAME=firefox
+
+	cd $SCRIPT_PATH
+
+	TMP_DIR=$(mktemp --directory)
+
+	sed -e "s,<EXEC_LOCATION>,firefox,g" \
+		-e "s,<ICON_LOCATION>,${PATH_LOGO},g" "${SCRIPT_PATH}/linux/desktop.template" >"${TMP_DIR}/${RESOURCE_NAME}.desktop"
+
+	mkdir -p "${HOME}/.local/share/applications"
+	cp "${TMP_DIR}/${RESOURCE_NAME}.desktop" "${HOME}/.local/share/applications/"
+
+	mkdir -p "${HOME}/.local/share/mime/packages"
+	cp "${SCRIPT_PATH}/linux/${RESOURCE_NAME}.xml" "${HOME}/.local/share/mime/packages"
+
+	rm "${TMP_DIR}/${RESOURCE_NAME}.desktop"
+	rmdir "${TMP_DIR}"
+
+	if [ -d "${HOME}/.local/share/applications" ]; then
+		if command -v update-desktop-database >/dev/null; then
+			update-desktop-database "${HOME}/.local/share/applications"
+		fi
+	fi
+
+	if [ -d "${HOME}/.local/share/mime" ]; then
+		if command -v update-mime-database >/dev/null; then
+			update-mime-database "${HOME}/.local/share/mime"
+		fi
+	fi
+}
+
+# remove-firefox-esr
+# install
+# desktop-file
+update-firefox
