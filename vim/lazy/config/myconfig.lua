892f7774e1d4ac543c6b4277860bff649826a6ca
Johnny Cubides <jgcubidesc@gmail.com>
2024-03-20 21:49:52 -0500

    update

--- vim/lazy/config/myconfig.lua
+++ vim/lazy/config/myconfig.lua
@@ -1,6 +1,16 @@
 -- Iniciar algunas funciones automáticamente en función del filetype
 require("config.autocmd")
 
+-- Configuración para el plugin de verible
+require("config.verible_lsp_config")
+
+-- Config snippets plugin
+-- https://github.com/smjonas/snippet-converter.nvim/blob/main/doc/documentation.md#luasnip
+-- plugin from_vscode
+require("luasnip.loaders.from_vscode").load({ paths = "~/projects/swissknife/vim/snippets/ultisnips2luasnip" })
+-- Config my snippets
+require("luasnip.loaders.from_lua").load({ paths = "~/projects/swissknife/vim/snippets/luasnippets" })
+
 local function file_exists(file)
 	local f = io.open(file, "r")
 	if f then
@@ -11,20 +21,18 @@ local function file_exists(file)
 	end
 end
 
-if file_exists("./pro.vim") then
-	print("existe ./pro.vim")
-	vim.cmd("source ./pro.vim")
-elseif file_exists("./.pro.vim") then
-	print("existe ./.pro.vim")
-	vim.cmd("source ./.pro.vim")
+-- if file_exists("./pro.vim") then
+-- 	print("existe ./pro.vim")
+-- 	vim.cmd("source ./pro.vim")
+-- elseif file_exists("./.pro.vim") then
+-- 	print("existe ./.pro.vim")
+-- 	vim.cmd("source ./.pro.vim")
+-- end
+
+if file_exists("./pro.lua") then
+	print("existe ./pro.lua")
+	require("./pro")
+elseif file_exists("./.pro.lua") then
+	print("existe ./.pro.lua")
+	require("./.pro")
 end
-
--- Configuración para el plugin de verible
-require("config.verible_lsp_config")
-
--- Config snippets plugin
--- https://github.com/smjonas/snippet-converter.nvim/blob/main/doc/documentation.md#luasnip
--- plugin from_vscode
-require("luasnip.loaders.from_vscode").load({ paths = "~/projects/swissknife/vim/snippets/ultisnips2luasnip" })
--- Config my snippets
-require("luasnip.loaders.from_lua").load({ paths = "~/projects/swissknife/vim/snippets/luasnippets" })
