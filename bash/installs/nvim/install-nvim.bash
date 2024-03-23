990db4244fc9c935217d2392cc2b7b52de7dfd6d
Johnny Cubides <jgcubidesc@gmail.com>
2024-03-22 20:11:19 -0500

    update docs

--- bash/installs/nvim/install-nvim.bash
+++ bash/installs/nvim/install-nvim.bash
@@ -4,7 +4,9 @@ PD=~/gitPackages
 APP=$PD/nvim
 
 dependencies() {
-  sudo apt remove neovim
+	sudo apt remove neovim
+	sudo rm -rf /usr/local/bin/nvim
+	sudo rm -rf /usr/local/share/nvim
 	sudo apt update
 	sudo apt install \
 		make \
@@ -26,34 +28,54 @@ build() {
 	# Docs: https://github.com/neovim/neovim/blob/master/BUILD.md
 	cd $APP
 	cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
-  check
+	check
 	cd build && cpack -G DEB
-  check
+	check
 }
 
-install(){
-  cd $APP/neovim/build/
-  sudo dpkg -i nvim-linux64.deb
-  check
+install() {
+	cd $APP/neovim/build/
+	sudo dpkg -i nvim-linux64.deb
+	check
+}
+
+update() {
+	cd $APP/neovim
+	git pull
+	build
+	echo "puede ahora dar la orden de instalar"
 }
 
 autoremove() {
 	rm -rf $APP
 }
 
-check(){
-  if [[ $? -ne 0 ]] ; then
-    exit $?
-  fi
+check() {
+	if [[ $? -ne 0 ]]; then
+		exit $?
+	fi
+}
+
+help() {
+	echo "
+dependencies
+download
+build
+install
+autoremove
+  "
 }
 
 all() {
 	dependencies
 	download
 	build
+	install
 	autoremove
 }
 
 if [[ -v 1 ]]; then
 	$1
+else
+	help
 fi
