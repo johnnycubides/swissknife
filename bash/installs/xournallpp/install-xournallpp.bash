3298e70b2f70388e188abca5b0571a4d990d5e72
Johnny Cubides <jgcubidesc@gmail.com>
2024-03-23 10:39:07 -0500

    update script

--- bash/installs/xournallpp/install-xournallpp.bash
+++ bash/installs/xournallpp/install-xournallpp.bash
@@ -1,10 +1,30 @@
+#!/bin/bash
+
+APP=xournalpp
+DOWNLOAD=https://github.com/xournalpp/xournalpp/releases/download/v1.2.3/xournalpp-1.2.3-Debian-bullseye-x86_64.deb
+
 dependencies() {
+	sudo apt update
 	sudo apt install \
 		libportaudio2 libportaudiocpp0 libzip4 \
 		-y
 }
 
 install() {
-	wget https://github.com/xournalpp/xournalpp/releases/download/v1.2.3/xournalpp-1.2.3-Debian-bullseye-x86_64.deb
-
+	wget -O $APP.deb $DOWNLOAD
+	sudo dpkg -i $APP.deb
+	rm -f $APP.deb
 }
+
+help() {
+	echo "
+dependencies
+install
+  "
+}
+
+if [[ -v 1 ]]; then
+	$1
+else
+	help
+fi
