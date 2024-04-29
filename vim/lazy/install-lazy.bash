bb75abe755f79c9a315dbe416e9d4cf0613e9249
Johnny Cubides <jgcubidesc@gmail.com>
2024-04-29 11:14:20 -0500

    update

--- vim/lazy/install-lazy.bash
+++ vim/lazy/install-lazy.bash
@@ -69,16 +69,16 @@ config() {
 	# cp ./config/verible_lsp_config.lua $CONFIG_LUA/
 
 	rm $PLUG_LUA/typescript-tools.lua
+	rm $PLUG_LUA/aerial.lua
+	rm $PLUG_LUA/plantuml-syntax.lua
+	rm $PLUG_LUA/plantuml.lua
 	# rm $PLUG_LUA/vue-language-tools.lua
 	# cp $PLUG_LUA/example.lua ./plugins/
 	# rm $PLUG_LUA/example.lua
 	# rm $PLUG_LUA/coc.lua
 	# rm $PLUG_LUA/verible.lua
 	# rm $PLUG_LUA/snippet-converter.lua
-	# rm $PLUG_LUA/plantuml.lua
-	# rm $PLUG_LUA/plantuml-syntax.lua
 	# rm $PLUG_LUA/markdown-preview.lua
-	# rm $PLUG_LUA/aerial.lua
 	# rm $PLUG_LUA/gentags.lua
 	# rm $PLUG_LUA/treesitter.lua
 	# rm $PLUG_LUA/mason.lua
@@ -86,15 +86,16 @@ config() {
 	# rm $PLUG_LUA/verilog_systemverilog.lua
 
 	cp ./plugins/typescript-tools.lua $PLUG_LUA/ # Este es el plugin adecuado
+	cp ./plugins/aerial.lua $PLUG_LUA/
+	cp ./plugins/plantuml-syntax.lua $PLUG_LUA/
+	cp ./plugins/plantuml.lua $PLUG_LUA/
 	# cp ./plugins/vue-language-tools.lua $PLUG_LUA/ # este requiere tsserver, se requiere buscar mejoras para que funcione con typescript-tools
 	# cp ./plugins/coc.lua $PLUG_LUA/
 	# cp ./plugins/verible.lua $PLUG_LUA/
 	# cp ./plugins/snippet-converter.lua $PLUG_LUA/
 	# cp ./plugins/plantuml.lua $PLUG_LUA/
-	# cp ./plugins/plantuml-syntax.lua $PLUG_LUA/
 	# cp ./plugins/treesitter.lua $PLUG_LUA/
 	# cp ./plugins/markdown-preview.lua $PLUG_LUA/
-	# cp ./plugins/aerial.lua $PLUG_LUA/
 	# cp ./plugins/gentags.lua $PLUG_LUA/
 	# cp ./plugins/ultisnips.lua $PLUG_LUA/
 	# cp ./plugins/verilog_systemverilog.lua $PLUG_LUA/
