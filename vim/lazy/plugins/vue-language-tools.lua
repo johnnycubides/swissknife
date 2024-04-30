9a735cf246a9a568aa755318abf1c05fe8eb3210
Johnny Cubides <jgcubidesc@gmail.com>
2024-04-30 01:15:57 -0500

    update

--- vim/lazy/plugins/vue-language-tools.lua
+++ vim/lazy/plugins/vue-language-tools.lua
@@ -5,6 +5,7 @@ return {
 		config = function()
 			-- https://github.com/williamboman/mason-lspconfig.nvim/issues/371
 			-- https://github.com/Jarmos-san/dotfiles/blob/5cd6aa506d9888e4437340741def5833148cfba5/dotfiles/.config/nvim/lua/plugins/lspconfig.lua#L70
+			-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#volar
 			local capabilities =
 				require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 			local lspconfig = require("lspconfig")
