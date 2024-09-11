c0c4692b1510e12e30515bbb04a33b19d37482c8
Johnny Cubides <jgcubidesc@gmail.com>
2024-09-10 19:55:36 -0500

    update config

---
+++ bash/installs/lite-xl/install-all.bash
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+TMP=/tmp/lite-xl
+URL_CONFIG=
+LITEX_CONFIG=lite-xl-config
+
+download() {
+  rm -rf $TMP
+  mkdir -p $TMP
+  cd $TMP
+  wget $URL_CONFIG/${LITEX_CONFIG}.tar.gz
+  tar xvf $LITEX_CONFIG.tar.gz
+}
+
+install-lite() {
+  cd $TMP
+  bash lite-xl-install.bash dependencies
+  check
+  bash lite-xl-install.bash remove
+  bash lite-xl-install.bash install
+  check
+  bash lite-xl-install.bash lsp-install
+  check
+  bash lite-xl-install.bash build-install
+  check
+  bash lite-xl-install.bash config
+  check
+  bash lite-xl-install.bash myconfig
+  check
+  bash lite-xl-install.bash mkdesktop
+  check
+}
+
+all() {
+  install-all
+}
+
+pack() {
+  tar cvf lite-xl-config.tar.gz install-desktop.sh linux lite-xl-install.bash logo.svg myconfig.lua plugins
+}
+
+check() {
+  if [[ $? -ne 0 ]]; then
+    exit $?
+  fi
+}
+
+help() {
+  echo "all"
+}
+
+if [[ -v 1 ]]; then
+  $1
+else
+  help
+fi
