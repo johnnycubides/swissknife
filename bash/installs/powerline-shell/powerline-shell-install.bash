c321e0df3f81c9741f7e536cf2395127ee4fb5a4
Johnny Cubides <jgcubidesc@gmail.com>
2024-10-18 10:57:04 -0500

    update python 3.12

--- bash/installs/powerline-shell/powerline-shell-install.bash
+++ bash/installs/powerline-shell/powerline-shell-install.bash
@@ -1,31 +1,37 @@
 DP=~/gitPackages
 
+SCRIPT_PATH=$(
+  cd $(dirname $0)
+  pwd
+)
+
 dependencies() {
-	sudo apt update
-	sudo apt install python3-setuptools fonts-powerline -y
+  sudo apt update
+  sudo apt install python3-setuptools fonts-powerline -y
 }
 
 install() {
-	cd $DP
-	git clone https://github.com/b-ryan/powerline-shell.git
-	cd powerline-shell
-	sudo python3 setup.py install --record files.txt
-	cd ..
-	sudo rm -rf powerline-shell
+  cd $DP
+  git clone https://github.com/b-ryan/powerline-shell.git
+  cd powerline-shell
+  patch -p1 -N <$SCRIPT_PATH/fix.patch # https://github.com/b-ryan/powerline-shell/issues/546
+  sudo python3 setup.py install --record files.txt
+  cd ..
+  sudo rm -rf powerline-shell
 }
 
 config_laptop() {
-	mkdir -p ~/.config/powerline-shell/
-	cp ./config_laptop.json ~/.config/powerline-shell/config.json
+  mkdir -p ~/.config/powerline-shell/
+  cp ./config_laptop.json ~/.config/powerline-shell/config.json
 }
 
 config_desktop() {
-	mkdir -p ~/.config/powerline-shell/
-	cp ./config_desktop.json ~/.config/powerline-shell/config.json
+  mkdir -p ~/.config/powerline-shell/
+  cp ./config_desktop.json ~/.config/powerline-shell/config.json
 }
 
 bash_config() {
-	cat ./bashrc_powerline_shell.bash >>~/.bashrc
+  cat ./bashrc_powerline_shell.bash >>~/.bashrc
 }
 
 # dependencies
@@ -35,7 +41,7 @@ bash_config() {
 # bash_config # Esta configuracion se debe realizar una unica vez
 
 help() {
-	echo "
+  echo "
 dependencies
 install
 config_laptop
@@ -45,7 +51,7 @@ bash_config # Esta configuracion se debe realizar una unica vez
 }
 
 if [[ -v 1 ]]; then
-	$1
+  $1
 else
-	help
+  help
 fi
