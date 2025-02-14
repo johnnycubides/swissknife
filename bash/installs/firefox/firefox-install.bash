67f17def578f2ad2e006fd51f7d6aa4420ae3c26
Johnny Cubides <jgcubidesc@gmail.com>
2025-02-14 09:13:32 -0500

    update

--- bash/installs/firefox/firefox-install.bash
+++ bash/installs/firefox/firefox-install.bash
@@ -3,64 +3,67 @@
 SCRIPT_PATH=$(pwd)
 
 remove-firefox-esr() {
-	sudo apt remove firefox-esr -y
+  sudo apt remove firefox-esr -y
 }
 
+APP=firefox
+PACKAPP=$APP.tar.xz
+
 DP=~/gitPackages
-DAPP=$DP/firefox
+DAPP=$DP/$APP
 DOWNLOAD="https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US"
 
 install() {
-	rm -rf $DAPP
-	sudo rm /usr/local/bin/firefox
-	mkdir -p $DAPP
-	cd $DAPP
-	wget -O firefox.tar.bz2 $DOWNLOAD
-	tar xvjf firefox.tar.bz2
-	rm -f firefox.tar.bz2
-	cd firefox
-	sudo ln -sr firefox /usr/local/bin/firefox
+  rm -rf $DAPP
+  sudo rm /usr/local/bin/$APP
+  mkdir -p $DAPP
+  cd $DAPP
+  wget -O $PACKAPP $DOWNLOAD
+  tar xvf $PACKAPP
+  rm -f $PACKAPP
+  cd $APP/
+  sudo ln -sr $APP /usr/local/bin/$APP
 }
 
 update-firefox() {
-	cd $DAPP
-	wget -O firefox.tar.bz2 $DOWNLOAD
-	tar xvjf firefox.tar.bz2
-	rm -f firefox.tar.bz2
+  cd $DAPP
+  wget -O $PACKAPP $DOWNLOAD
+  tar xvf $PACKAPP
+  rm -f $PACKAPP
 }
 
 desktop-file() {
 
-	PATH_LOGO=$DAPP/firefox/browser/chrome/icons/default/default128.png
-	RESOURCE_NAME=firefox
+  PATH_LOGO=$DAPP/firefox/browser/chrome/icons/default/default128.png
+  RESOURCE_NAME=firefox
 
-	cd $SCRIPT_PATH
+  cd $SCRIPT_PATH
 
-	TMP_DIR=$(mktemp --directory)
+  TMP_DIR=$(mktemp --directory)
 
-	sed -e "s,<EXEC_LOCATION>,firefox,g" \
-		-e "s,<ICON_LOCATION>,${PATH_LOGO},g" "${SCRIPT_PATH}/linux/desktop.template" >"${TMP_DIR}/${RESOURCE_NAME}.desktop"
+  sed -e "s,<EXEC_LOCATION>,firefox,g" \
+    -e "s,<ICON_LOCATION>,${PATH_LOGO},g" "${SCRIPT_PATH}/linux/desktop.template" >"${TMP_DIR}/${RESOURCE_NAME}.desktop"
 
-	mkdir -p "${HOME}/.local/share/applications"
-	cp "${TMP_DIR}/${RESOURCE_NAME}.desktop" "${HOME}/.local/share/applications/"
+  mkdir -p "${HOME}/.local/share/applications"
+  cp "${TMP_DIR}/${RESOURCE_NAME}.desktop" "${HOME}/.local/share/applications/"
 
-	mkdir -p "${HOME}/.local/share/mime/packages"
-	cp "${SCRIPT_PATH}/linux/${RESOURCE_NAME}.xml" "${HOME}/.local/share/mime/packages"
+  mkdir -p "${HOME}/.local/share/mime/packages"
+  cp "${SCRIPT_PATH}/linux/${RESOURCE_NAME}.xml" "${HOME}/.local/share/mime/packages"
 
-	rm "${TMP_DIR}/${RESOURCE_NAME}.desktop"
-	rmdir "${TMP_DIR}"
+  rm "${TMP_DIR}/${RESOURCE_NAME}.desktop"
+  rmdir "${TMP_DIR}"
 
-	if [ -d "${HOME}/.local/share/applications" ]; then
-		if command -v update-desktop-database >/dev/null; then
-			update-desktop-database "${HOME}/.local/share/applications"
-		fi
-	fi
+  if [ -d "${HOME}/.local/share/applications" ]; then
+    if command -v update-desktop-database >/dev/null; then
+      update-desktop-database "${HOME}/.local/share/applications"
+    fi
+  fi
 
-	if [ -d "${HOME}/.local/share/mime" ]; then
-		if command -v update-mime-database >/dev/null; then
-			update-mime-database "${HOME}/.local/share/mime"
-		fi
-	fi
+  if [ -d "${HOME}/.local/share/mime" ]; then
+    if command -v update-mime-database >/dev/null; then
+      update-mime-database "${HOME}/.local/share/mime"
+    fi
+  fi
 }
 
 # remove-firefox-esr
@@ -69,7 +72,7 @@ desktop-file() {
 # update-firefox
 
 help() {
-	echo "
+  echo "
 remove-firefox-esr
 install
 desktop-file
@@ -78,7 +81,7 @@ update-firefox
 }
 
 if [[ -v 1 ]]; then
-	$1
+  $1
 else
-	help
+  help
 fi
