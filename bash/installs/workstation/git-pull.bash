938003c9db46c4c61dbcb5298327c9d25dcdeb0d
Johnny Cubides <jgcubidesc@gmail.com>
2025-03-11 09:52:46 -0500

    update

--- bash/installs/workstation/git-pull.bash
+++ bash/installs/workstation/git-pull.bash
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# cd ~/gitPackages/my-gnote
-# pwd
-# git pull
-# cd ~/projects/digital-electronic-1-101/
-# pwd
-# git pull
-# cd ~/projects/swissknife/
-# pwd
-# git pull
-
-dirPrj=("$HOME/gitPackages/my-gnote" "$HOME/projects/digital-electronic-1-101/" "$HOME/projects/swissknife/")
+dirPrj=(
+  "$HOME/gitPackages/my-gnote"
+  "$HOME/projects/digital-electronic-1-101/"
+  "$HOME/projects/swissknife/"
+)
 
 for runner in "${dirPrj[@]}"; do
   cd $runner
