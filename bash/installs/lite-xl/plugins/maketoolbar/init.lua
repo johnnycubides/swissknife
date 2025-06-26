29868e974cc400e35c53d6c6d2b219224d4fca51
Johnny Cubides <jgcubidesc@gmail.com>
2025-06-26 11:12:11 -0500

    update

--- bash/installs/lite-xl/plugins/maketoolbar/init.lua
+++ bash/installs/lite-xl/plugins/maketoolbar/init.lua
@@ -63,10 +63,102 @@ end
 local commandList = {
 	{ label = "synthesis", command = "make clean syn" },
 	{ label = "firmware", command = "make clean c-build" },
-	{ label = "config", command = "config" },
-	{ label = "Simulation", command = "sym" },
-	{ label = "diagram", command = "rtl" },
-	{ label = "help", command = "help" },
+	{ label = "config", command = "make config" },
+	{ label = "Simulation", command = "make sym" },
+	{ label = "diagram", command = "make rtl" },
+	{ label = "help", command = "make help" },
+}
+
+-- local symbolList = {
+-- 	{ name = "play", icon = "" }, -- 1
+-- 	{ name = "download", icon = "" },
+-- 	{ name = "trash", icon = "" },
+-- 	{ name = "restart", icon = "" },
+-- 	{ name = "scissors", icon = "" }, -- 5
+-- 	{ name = "search", icon = "" },
+-- 	{ name = "check", icon = "" },
+-- 	{ name = "build", icon = "" },
+-- 	{ name = "wrench", icon = "" },
+-- 	{ name = "gear", icon = "" }, -- 10
+-- 	{ name = "eject", icon = "" },
+-- 	{ name = "stop", icon = "" },
+-- 	{ name = "pause", icon = "" },
+-- 	{ name = "docs", icon = "" },
+-- 	{ name = "checkbox", icon = "" }, -- 15
+-- 	{ name = "upload", icon = "" },
+-- 	{ name = "download1", icon = "" },
+-- 	{ name = "keyboard", icon = "" },
+-- 	{ name = "merge", icon = "" },
+-- 	{ name = "download2", icon = "" }, -- 20
+-- 	{ name = "git1", icon = "" },
+-- 	{ name = "branch", icon = "" },
+-- 	{ name = "git2", icon = "" },
+-- 	{ name = "git3", icon = "" },
+-- 	{ name = "git4", icon = "" }, -- 25
+-- 	{ name = "terminal_dark", icon = "" },
+-- 	{ name = "magic", icon = "" },
+-- 	{ name = "oddometer", icon = "" },
+-- 	{ name = "git5", icon = "" },
+-- 	{ name = "terminal_light", icon = "" }, -- 30
+-- 	{ name = "question", icon = "" },
+-- 	{ name = "block", icon = "" },
+-- 	{ name = "penguin", icon = "" },
+-- 	{ name = "bug", icon = "" },
+-- 	{ name = "pdf", icon = "" }, -- 35
+-- 	{ name = "img", icon = "" },
+-- 	{ name = "git", icon = "" },
+-- 	{ name = "connect", icon = "" },
+-- 	{ name = "desktop", icon = "" },
+-- 	{ name = "usb", icon = "" }, -- 40
+-- 	{ name = "hash", icon = "" },
+-- 	{ name = "bluetooth", icon = "" },
+-- 	{ name = "chip", icon = "" },
+-- }
+
+local symbolList = {
+	play = "", -- 1
+	download = "",
+	trash = "",
+	restart = "",
+	scissors = "", -- 5
+	search = "",
+	check = "",
+	build = "",
+	wrench = "",
+	gear = "", -- 10
+	eject = "",
+	stop = "",
+	pause = "",
+	docs = "",
+	checkbox = "", -- 15
+	upload = "",
+	download1 = "",
+	keyboard = "",
+	merge = "",
+	download2 = "", -- 20
+	git1 = "",
+	branch = "",
+	git2 = "",
+	git3 = "",
+	git4 = "", -- 25
+	terminal_dark = "",
+	magic = "",
+	oddometer = "",
+	git5 = "",
+	terminal_light = "", -- 30
+	question = "",
+	block = "",
+	penguin = "",
+	bug = "",
+	pdf = "", -- 35
+	img = "",
+	git = "",
+	connect = "",
+	desktop = "",
+	usb = "", -- 40
+	hash = "",
+	bluetooth = "",
+	chip = "",
 }
 
 command.add(nil, {
@@ -110,12 +202,12 @@ function Toolbar:new()
 	self.toolbar_font =
 		renderer.font.load(get_plugin_directory() .. PATHSEP .. "maketoolbar.ttf", style.icon_big_font:get_size())
 	self.toolbar_commands = {
-		{ symbol = "", command = commandList[1].label },
-		{ symbol = "", command = commandList[2].label },
-		{ symbol = "", command = commandList[3].label },
-		{ symbol = "", command = commandList[4].label },
-		{ symbol = "", command = commandList[5].label },
-		{ symbol = "", command = commandList[6].label },
+		{ symbol = symbolList.chip, command = commandList[1].label },
+		{ symbol = symbolList.build, command = commandList[2].label },
+		{ symbol = symbolList.download, command = commandList[3].label },
+		{ symbol = symbolList.bug, command = commandList[4].label },
+		{ symbol = symbolList.block, command = commandList[5].label },
+		{ symbol = symbolList.question, command = commandList[6].label },
 	}
 end
 
