410d3e101a7cd0a6f27db28d58890686c550e54f
Johnny Cubides <jgcubidesc@gmail.com>
2025-08-07 11:35:26 -0500

    update

--- bash/installs/starship/starship.bash
+++ bash/installs/starship/starship.bash
@@ -4,6 +4,23 @@ install() {
   curl -sS https://starship.rs/install.sh | sh
 }
 
-config() {
+bashrc() {
   echo 'eval "$(starship init bash)"' >>~/.bashrc
 }
+
+config() {
+  mkdir -p ~/.config && cp ./starship.toml ~/.config/
+}
+
+help() {
+  echo "install # instalar plugin desde crates
+bashrc # Agregar al bashrc una ÚNICA VEZ la configuración
+config # Agregar configuraci+on personalizada
+help"
+}
+
+if [[ -v 1 ]]; then
+  $1
+else
+  help
+fi
