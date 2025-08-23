69aa732b117f97989a46d61f4be1f3f834fb1b4d
Johnny Cubides <jgcubidesc@gmail.com>
2025-08-23 03:35:26 -0500

    update

--- bash/installs/plantuml/install-plantuml.bash
+++ bash/installs/plantuml/install-plantuml.bash
@@ -4,37 +4,37 @@
 LBIN=/usr/local/bin
 APP=plantuml
 DAPP=~/gitPackages/$APP
-VERSION=1.2024.3
-DOWNLOAD=https://github.com/plantuml/plantuml/releases/download/v1.2024.3/plantuml-$VERSION.jar
+VERSION=1.2025.4
+DOWNLOAD=https://github.com/plantuml/plantuml/releases/download/v$VERSION/plantuml-$VERSION.jar
 
 update() {
-	sudo apt update
+  sudo apt update
 }
 
 dependencies() {
-	sudo apt install \
-		graphviz \
-		-y
+  sudo apt install \
+    graphviz \
+    -y
 }
 
 install() {
-	rm -rf $DAPP
-	sudo rm -i $LBIN/$APP
-	mkdir -p $DAPP
-	cp plantuml.bash $DAPP
-	cd $DAPP
-	wget -O plantuml.jar $DOWNLOAD
-	sudo ln -sr plantuml.bash /usr/local/bin/$APP
+  rm -rf $DAPP
+  sudo rm -i $LBIN/$APP
+  mkdir -p $DAPP
+  cp plantuml.bash $DAPP
+  cd $DAPP
+  wget -O plantuml.jar $DOWNLOAD
+  sudo ln -sr plantuml.bash /usr/local/bin/$APP
 }
 
 help() {
-	echo "
+  echo "
 install
   "
 }
 
 if [[ -v 1 ]]; then
-	$1
+  $1
 else
-	help
+  help
 fi
