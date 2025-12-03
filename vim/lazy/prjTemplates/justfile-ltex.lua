631d810026503bd038640e456ea49ff3367eb1ae
Johnny Cubides <jgcubidesc@gmail.com>
2025-12-02 21:51:54 -0500

    update

--- vim/lazy/prjTemplates/justfile-ltex.lua
+++ vim/lazy/prjTemplates/justfile-ltex.lua
@@ -25,7 +25,6 @@ vim.keymap.set("n", "<F7>", help, { desc = "Help project" })
 -- archivo de configuración que se encuentra en ./.vim/coc-settings.json
 local lspconfig = require("lspconfig")
 lspconfig.ltex.setup({
-	autostart = false,
 	settings = {
 		ltex = {
 			language = "es",
