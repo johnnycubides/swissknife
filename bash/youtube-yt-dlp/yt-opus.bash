00c14a0bc979b2a547ccae5cb40d8fec4c560ec2
Johnny Cubides <jgcubidesc@gmail.com>
2026-06-16 08:12:45 -0500

    update

--- bash/youtube-yt-dlp/yt-opus.bash
+++ bash/youtube-yt-dlp/yt-opus.bash
@@ -1,4 +1,22 @@
 #!/bin/bash
 
 # conda activate work
-yt-dlp --extract-audio --audio-format opus $1
+# yt-dlp --extract-audio --audio-format opus $1
+
+# conda activate work
+yt-dlp \
+  --remote-components ejs:github \
+  --js-runtimes node \
+  --cookies-from-browser firefox \
+  --extract-audio --audio-format opus \
+  "$1"
+
+# conda activate work
+# # Exactly
+# yt-dlp \
+#   --remote-components ejs:github \
+#   --js-runtimes node:/home/johnny/.nvm/versions/node/v24.16.0/bin/node \
+#   --cookies-from-browser firefox \
+#   -f "bestvideo+bestaudio" \
+#   -v \
+#   "https://www.youtube.com/watch?v=ZOOCtEpGZG0"
