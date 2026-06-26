1a72dd480856982e5fff16b109ffe447357a0a40
Johnny Cubides <jgcubidesc@gmail.com>
2026-06-26 12:09:13 -0500

    update

---
+++ bash/installs/kicad/README.md
@@ -0,0 +1,4 @@
+Kicad funciona en x11, para wayland no hay soporte adecuado.
+
+Instala `./kicad-install.bash` ejecuta `kicad-x11` para darle soporte
+para x11
--- bash/installs/kicad/kicad-install.bash
+++ bash/installs/kicad/kicad-install.bash
@@ -5,15 +5,22 @@
 # }
 
 update() {
-	sudo apt update
+  sudo apt update
 }
 
 install() {
-	sudo apt install \
-		kicad \
-		kicad-packages3d \
-		-y
+  sudo apt install \
+    kicad \
+    kicad-packages3d \
+    -y
+}
+
+kicad-x11-ln() {
+  LN=kicad-x11
+  sudo rm -rf /usr/local/bin/$LN
+  sudo ln -sr ./kicad-x11.bash /usr/local/bin/$LN
 }
 
 # apt-add
 install
+kicad-x11-ln
